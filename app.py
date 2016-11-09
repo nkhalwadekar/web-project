c7f2c173ae0318e85c751f6fa9db1eae4b81e84d
Neha Khalwadekar <nehakhalwadekar@Nehas-MacBook-Pro.local>
2016-11-09 16:37:49 -0500

    added echo resource

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+
+    data = {
+        "name": "joe"
+    }
+
+    return jsonify(data)
+
+@app.route("/echo", methods=['POST', 'GET'])
+def echo():
+    incoming_json = request.get_json()
+
+    if incoming_json is None:
+        return abort(404)
+
+    return jsonify(incoming_json)
 
 if __name__ == "__main__":
     app.run()
